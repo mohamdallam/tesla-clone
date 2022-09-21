ad80fed2742109c99b45cc1a721e7ed9f3ccdf21
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-21 03:08:48 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useState } from "react";
+
+import Header from "./components/Header";
+import { BrowserRouter, Router, Route } from "react-router-dom";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Tesla Clone</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      </div>
+    </BrowserRouter>
   );
 }
 
