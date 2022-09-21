473b3ad6dc3b1d675bef06fadf5abcff3e4909ae
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-21 16:39:40 +0200

    login

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import { useState } from "react";
 import Header from "./layout/Header";
 import Menu from "./layout/Menu";
 
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderBlock from "./layout/HeaderBlock";
+import Login from "./layout/Login";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-        {isMenuOpen && <Menu />}
-        <HeaderBlock />
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+              {isMenuOpen && <Menu />}
+              <HeaderBlock />
+            </div>
+          }
+        />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/ButtonPrimary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/ButtonSecondary.css";
+
+function ButtonPrimary({ name, type, onClick }) {
+  return (
+    <button className="buttonPrimary" onClick={onClick} type={type}>
+      {name}
+    </button>
+  );
+}
+
+export default ButtonPrimary;
